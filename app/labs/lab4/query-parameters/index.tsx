2ae60eddef0c084ac709be346b1f264fbdbc9cf0
frankatank4526 <frankatank1@gmail.com>
2026-03-17 04:18:02 -0400

    trying more fixes with suspense

--- app/labs/lab4/query-parameters/index.tsx
+++ app/labs/lab4/query-parameters/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -49,9 +49,11 @@ export default function UrlEncoding() {
        className="form-control"
      />
      <h4>Programmatic navigation (using router.push):</h4>
+     <Suspense>
      <button onClick={goToQueryVersion} className="btn btn-success w-100">
        {a} + {b} → Query Params (programmatic)
      </button>
+     </Suspense>
      <button onClick={goToPathVersion} className="btn btn-success w-100 mt-2">
        {a} + {b} → Path Params (programmatic)
      </button>
