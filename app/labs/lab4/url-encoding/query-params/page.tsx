c98822088a257ad26ba66cbe6fb5225cb0d44987
frankatank4526 <frankatank1@gmail.com>
2026-03-17 04:31:45 -0400

    maybe this works?

--- app/labs/lab4/url-encoding/query-params/page.tsx
+++ app/labs/lab4/url-encoding/query-params/page.tsx
@@ -2,7 +2,7 @@
 
 
 import { useSearchParams } from "next/navigation";
-
+export const dynamic = "force-dynamic";
 
 export default function QueryCalculator() {
  const searchParams = useSearchParams();
