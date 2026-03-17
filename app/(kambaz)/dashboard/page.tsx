9b99267fb02816e817b223e12a252e33b9833311
frankatank4526 <frankatank1@gmail.com>
2026-03-17 04:06:34 -0400

    fixing stuff

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -42,6 +42,7 @@ export default function Dashboard() {
                     {courses.filter((course) =>
                         enrollments.some(
                             (enrollment) => 
+                                enrollment !== null && course !== null &&
                                 enrollment.user === (currentUser as any)._id &&
                                 enrollment.course === course._id
                         )).map((course) => (
